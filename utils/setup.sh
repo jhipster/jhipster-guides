351ed8a567351a733a4350347635bc516945552c
Jérôme <hawkurane@gmail.com>
2019-02-27 15:20:14 +0100

    Added a git ignore for gatling generated files and fixed setup script

--- utils/setup.sh
+++ utils/setup.sh
@@ -2,8 +2,6 @@
 
 echo "====================> UPGRADING NPM"
 npm install -g npm;
-echo "====================> INSTALLING YEOMAN"
-npm install -g yo;
 echo "====================> INSTALLING JHIPSTER"
 npm install -g generator-jhipster;
 
